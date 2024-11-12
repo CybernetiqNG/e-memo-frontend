408117f8fc6374bc7484a8b563acb5915464f49b
Aholu Samuel <samuelaholu15@gmail.com>
2024-11-12 13:01:03 +0100

    Uupdates

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -219,7 +219,7 @@ const SideBar = () => {
               >
                 <div className="inline-flex">
                   <img src={auth ? Auth2 : Auth} alt="Authentication Icon" />
-                  <p className="ml-3">Authentication</p>
+                  <p className="ml-3">Authentications</p>
                 </div>
                 <div>{!auth ? <FaAngleDown /> : <FaAngleUp />}</div>
               </div>
